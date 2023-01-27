6b4200603ef1ab420fc3229b283a1ad3e0fa7212
Nathan Angelo B. Cruz <jonathan.a.b.cross@gmail.com>
2023-01-28 01:00:19 +0800

    Add the random pick function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_spinbox/material.dart';
 import 'package:provider/provider.dart';
+import 'package:quantity_input/quantity_input.dart';
 
 void main() {
   runApp(
@@ -43,11 +45,12 @@ class ItemListModel extends ChangeNotifier {
     _items[index] = newItem;
     notifyListeners();
   }
-  void _print() {
-    for (var item in items) {
-      print(item.name);
-    }
-  }
+
+  // void _print() {
+  //   for (var item in items) {
+  //     print(item.name);
+  //   }
+  // }
 }
 
 class MyApp extends StatelessWidget {
@@ -73,20 +76,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _textField = '';
-
-  void editTextField(String value) {
-    setState(() {
-      _textField = value;
-    });
-  }
-
   void addToList(BuildContext context, Item item) {
     var itemList = context.read<ItemListModel>();
     itemList.add(item);
   }
 
   final _textFieldController = TextEditingController();
+  int _numToPick = 1;
+
+  List<Item> pickFromList(BuildContext context, int quantity) {
+    final itemList = context.read<ItemListModel>();
+    final items = [...itemList.items];
+    items.shuffle();
+    return items.sublist(0, quantity - 1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -98,9 +101,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
-              // onChanged: (value) => editTextField(value),
               controller: _textFieldController,
               onSubmitted: (value) {
                 var item = Item(value);
@@ -116,31 +119,124 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             const SizedBox(height: 22.5),
-            Expanded(
-              child: ListView(
-                key: Key(itemList.items.length.toString()),
-                padding: const EdgeInsets.all(20),
-                children: [
-                  Column(
-                    children: [
-                      Consumer<ItemListModel>(
-                        builder: (context, item, child) => Column(
-                          children: [
-                            for (var i in item.items)
-                              ItemWidget(
-                                item: i,
-                                key: Key(i.name),
-                              )
-                          ],
-                        )
-                      ),
-                    ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  [
+                ElevatedButton.icon(
+                  // onPressed: onPressed,
+                  label: const Text('Pick'),
+                  icon: const Icon(Icons.casino),
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(const Size(100, 50)),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(24))),
                   ),
-                ],
-              ),
+                  onPressed: () => showDialog(
+                    context: context,
+                    builder: (dialogContext) => PickDialog(quantity: _numToPick)
+                  ),
+                ),
+                const SizedBox(width: 24),
+                QuantityInput(
+                  value: _numToPick, 
+                  onChanged: (value) => setState(() => _numToPick = int.parse(value)),
+                  minValue: 1,
+                  maxValue: itemList.items.isEmpty ? 1 : itemList.items.length,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                )
+              ],
             ),
+            const SizedBox(height: 22.5),
+            ItemListView(itemList: itemList),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class PickDialog extends StatelessWidget {
+  const PickDialog({
+    Key? key,
+    required this.quantity,
+  }) : super(key: key);
+
+  final int quantity;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final itemList = context.read<ItemListModel>();
+    final items = [...itemList.items];
+    items.shuffle();
+    final pickedItems = items.sublist(0, quantity);
+
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            for (var item in pickedItems)
+              Text(
+                item.name,
+                style: const TextStyle(fontSize: 24),
+              ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              }, 
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all(const Size(100, 40)),
+                textStyle: MaterialStateProperty.all(
+                  const TextStyle(
+                    fontSize: 20,
+                  )
+                ),
+              ),
+              child: const Text('Close'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemListView extends StatelessWidget {
+  const ItemListView({
+    Key? key,
+    required this.itemList,
+  }) : super(key: key);
+
+  final ItemListModel itemList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView(
+        key: Key(itemList.items.length.toString()),
+        padding: const EdgeInsets.all(20),
+        children: [
+          Column(
+            children: [
+              Consumer<ItemListModel>(
+                builder: (context, item, child) => Column(
+                  children: [
+                    for (var i in item.items)
+                      ItemWidget(
+                        item: i,
+                        key: Key(i.name),
+                      )
+                  ],
+                )
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
@@ -176,6 +272,7 @@ class _ItemWidgetState extends State<ItemWidget> {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 2,
       child: ListTile(
         title: Row(
           children: [
